647ca8464079604ed23c9c7ca3d06645b3f77ba7
Merlin Mon Mathew <40564473+merlin2410@users.noreply.github.com>
2023-06-27 11:50:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+	str = str.split(" ").join("");
+	if(str===str.reverse()){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 module.exports = palindrome
